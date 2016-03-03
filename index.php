e896aca5c24eefd9792bedaae02716d62b93a6a0
streetLAMP <streetlamp101@gmail.com>
2016-03-03 20:21:12 +0000

    adding gonzo and star wars img to foobar-star-wars clone

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hello world
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>home sweet home</title>
+		<!-- CSS: Framework -->
+	</head>
+<body>
+	<div id="container">
+	<b>hello to my cloud9 world with live preview</b>
+	<p><img src="starwars-1.jpg" alt="star wars" title="I like star wars" /></p>
+	<p>Gonzo should have been in Star Wars...<br /><img src="Gonzo-2011.png" alt="Gonzo" title="I like Gonzo" /></p>
+
+	</div> <!-- /container -->
+
+	<footer>
+		copyright me.
+	</footer>
+
+</body>
+</html>
---
+++ squares.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Squares</title>
+	</head>
+<body>
+	<div style="background-color:red;width:300px;height:300px;float:left;">red</div>
+	<div style="background-color:pink;width:300px;height:300px;float:left;">pink</div>
+	<div style="background-color:lime;width:300px;height:300px;float:left;">lime</div>
+	<div style="background-color:orange;width:300px;height:300px;float:left;">orange</div>
+</body>
+</html>
